7afcd5c84116150fdf705ba89f22bf83fa2405ee
SupineKrill <CollinRWilson@gmail.com>
2017-09-29 23:50:45 -0400

    First working version with music complete

--- krillbot.py
+++ krillbot.py
@@ -5,19 +5,46 @@ import aiohttp
 
 client = discord.Client()
 krill_player = None
+
+
 class player_obj:
+
     def __init__(self, channel_to_join):
         self.channel_to_join = channel_to_join
-    async def print_message(self):
-        print("Hello")
-    async def join_server(self):
-        voice_channel = await client.join_voice_channel(self.channel_to_join)
-        player = await voice_channel.create_ytdl_player('https://www.youtube.com/watch?v=3mUfOs-CTCw')
-        player.volume = 0.5
-        player.start()
-        print("Joined voice channel {}".format(channel))
-    async def print_server(self):
-        print(player)
+        self.player = None
+        self.voice_channel = None
+
+    async def play_song(self, song_url):
+        try:
+            self.player.is_playing()
+        except:
+            self.voice_channel = await client.join_voice_channel(self.channel_to_join)
+            self.player = await self.voice_channel.create_ytdl_player(song_url, after=my_after)
+            self.player.volume = 0.5
+            self.player.start()
+
+    async def stop_player(self):
+        self.player.stop()
+        await self.vc_disc()
+        print("disc")
+
+    async def vc_disc(self):
+        await self.voice_channel.disconnect()
+        self.player = None
+        self.voice_channel = None
+
+
+def my_after():
+    krill_player.player.stop()
+    coro = krill_player.voice_channel.disconnect()
+    fut = asyncio.run_coroutine_threadsafe(coro, client.loop)
+    krill_player.player = None
+    krill_player.voice_channel = None
+    try:
+        fut.result()
+    except:
+        pass
+
 
 @client.event
 async def on_ready():
@@ -30,36 +57,43 @@ async def on_ready():
             if str(channel.type) == "voice":
                 if not discord.opus.is_loaded():
                     discord.opus.load_opus('/usr/local/lib/libopus.so')
-                global krill_player 
+                global krill_player
                 krill_player = player_obj(channel)
-                await krill_player.print_message()
-                await krill_player.join_server()
-                await krill_player.print_server()
 
-#Main event for controlling all actions on message
+# Main event for controlling all actions on message
+
+
 @client.event
 async def on_message(message):
     if message.content == '`exit':
-        #Exit if user is SupineKrill#9842, otherwise alert
+        # Exit if user is SupineKrill#9842, otherwise alert
         if str(message.author) == "SupineKrill#9842":
             await client.send_message(message.channel, 'Exiting...')
             await client.close()
         else:
             await client.send_message(message.channel, "Only available for SupineKrill")
     elif message.content[:7] == "`google":
-        #Return I'm feeling lucky result from google
+        # Return I'm feeling lucky result from google
         message_content = (str(message.content)).split()[1:]
         if message_content != []:
-            url = await return_url("http://www.google.com/search?q={}&btnI".format('+'.join(message_content)))
+            url = await return_url(f"http://www.google.com/search?q={'+'.join(message_content)}&btnI")
             await client.send_message(message.channel, url)
+    elif message.content[:8] == "`youtube":
+        message_content = (str(message.content)).split()[1:]
+        if message_content != []:
+            url = await return_url(f"http://www.google.com/search?q={'+'.join(message_content)}+site%3Ayoutube.com&btnI")
+            await krill_player.play_song(url)
+    elif message.content == "`stop music":
+        await krill_player.stop_player()
+
 
 async def return_url(url):
     async with aiohttp.request('GET', url) as response:
         return response.url
 
-#Token and running bot stuff
+# Token and running bot stuff
 with open('token.txt', 'r') as myfile:
     client_token = myfile.read()
+print(client_token)
 client.run(client_token)
 client.close()
-    
